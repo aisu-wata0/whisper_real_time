c0a1ebf1d146e16c3dff5c269ef21aa17fed0c34
Bruno FS <bruno.serbena@gmail.com>
2023-02-23 14:02:39 -0300

    removed usage of temp_file & added windows mic choice & keyboard & prefix

--- transcribe_demo.py
+++ transcribe_demo.py
@@ -4,35 +4,57 @@ import argparse
 import io
 import os
 import speech_recognition as sr
-import whisper
-import torch
 
 from datetime import datetime, timedelta
 from queue import Queue
 from tempfile import NamedTemporaryFile
 from time import sleep
 from sys import platform
-
+import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="medium", help="Model to use",
                         choices=["tiny", "base", "small", "medium", "large"])
-    parser.add_argument("--non_english", action='store_true',
-                        help="Don't use the english model.")
+    parser.add_argument("--english", action='store_true',
+                        help="Use the english model.")
     parser.add_argument("--energy_threshold", default=1000,
                         help="Energy level for mic to detect.", type=int)
     parser.add_argument("--record_timeout", default=2,
                         help="How real time the recording is in seconds.", type=float)
-    parser.add_argument("--phrase_timeout", default=3,
+    parser.add_argument("--phrase_timeout", default=2,
                         help="How much empty space between recordings before we "
                              "consider it a new line in the transcription.", type=float)  
-    if 'linux' in platform:
-        parser.add_argument("--default_microphone", default='pulse',
-                            help="Default microphone name for SpeechRecognition. "
-                                 "Run this with 'list' to view available Microphones.", type=str)
+
+    parser.add_argument("--default_microphone", "-m", default='',
+                        help="Default microphone name for SpeechRecognition. "
+                                "Run this with 'list' to view available Microphones.", type=str)
+
+    parser.add_argument("--keyboard", default=False,
+                        help="Simulate typing the transcription with your keyboard.", type=bool)
+    
+    parser.add_argument("--prefix", default='',
+                        help="add prefix to the start of every sentence.", type=str)
+    parser.add_argument("--suffix", default='',
+                        help="add suffix to the end of every sentence", type=str)
+    
+    parser.add_argument("--sample_rate", default=16000,
+                        help="Input sample rate", type=int)
+    
+    parser.add_argument("--task", default=None,
+                        help="Whisper task (None to transcribe, or 'translate')", type=str)
+
     args = parser.parse_args()
     
+    task = None
+    if args.task:
+        task = args.task
+
+    keyboard = None
+    if args.keyboard:
+        from pynput.keyboard import Key, Listener, Controller as keyboard_controller
+        keyboard = keyboard_controller()
+
     # The last time a recording was retreived from the queue.
     phrase_time = None
     # Current raw audio bytes.
@@ -45,10 +67,14 @@ def main():
     # Definitely do this, dynamic energy compensation lowers the energy threshold dramtically to a point where the SpeechRecognizer never stops recording.
     recorder.dynamic_energy_threshold = False
     
+    record_timeout = args.record_timeout
+    phrase_timeout = args.phrase_timeout
+    
     # Important for linux users. 
     # Prevents permanent application hang and crash by using the wrong Microphone
-    if 'linux' in platform:
-        mic_name = args.default_microphone
+    mic_name = args.default_microphone
+    source = None
+    if mic_name:
         if not mic_name or mic_name == 'list':
             print("Available microphone devices are: ")
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
@@ -56,22 +82,17 @@ def main():
             return
         else:
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
+                print(name)
                 if mic_name in name:
-                    source = sr.Microphone(sample_rate=16000, device_index=index)
+                    source = sr.Microphone(sample_rate=args.sample_rate, device_index=index)
+                    print("found!")
                     break
     else:
-        source = sr.Microphone(sample_rate=16000)
-        
-    # Load / Download model
-    model = args.model
-    if args.model != "large" and not args.non_english:
-        model = model + ".en"
-    audio_model = whisper.load_model(model)
+        source = sr.Microphone(sample_rate=args.sample_rate)
 
-    record_timeout = args.record_timeout
-    phrase_timeout = args.phrase_timeout
-
-    temp_file = NamedTemporaryFile().name
+    if source is None:
+        raise Exception(f"Microphone not found: {mic_name}")
+    
     transcription = ['']
     
     with source:
@@ -91,6 +112,13 @@ def main():
     recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
 
     # Cue the user that we're ready to go.
+    # Load / Download model
+    import whisper
+    import torch
+    model = args.model
+    if args.model != "large" and args.english:
+        model = model + ".en"
+    audio_model = whisper.load_model(model)
     print("Model loaded.\n")
 
     while True:
@@ -114,27 +142,29 @@ def main():
 
                 # Use AudioData to convert the raw data to wav data.
                 audio_data = sr.AudioData(last_sample, source.SAMPLE_RATE, source.SAMPLE_WIDTH)
-                wav_data = io.BytesIO(audio_data.get_wav_data())
+                wav_data = audio_data.get_wav_data()
 
-                # Write wav data to the temporary file as bytes.
-                with open(temp_file, 'w+b') as f:
-                    f.write(wav_data.read())
+                data_s16 = np.frombuffer(wav_data, dtype=np.int16, count=len(wav_data)//2, offset=0)
+                float_data = data_s16.astype(np.float32, order='C') / 32768.0
 
                 # Read the transcription.
-                result = audio_model.transcribe(temp_file, fp16=torch.cuda.is_available())
+                result = audio_model.transcribe(float_data, fp16=torch.cuda.is_available(), task=task)
                 text = result['text'].strip()
 
                 # If we detected a pause between recordings, add a new item to our transcripion.
                 # Otherwise edit the existing one.
-                if phrase_complete:
+                if phrase_complete and text:
+                    text = args.prefix + text + args.suffix
                     transcription.append(text)
+                    if keyboard:     
+                        keyboard.type(text)
                 else:
                     transcription[-1] = text
 
                 # Clear the console to reprint the updated transcription.
-                os.system('cls' if os.name=='nt' else 'clear')
+                # os.system('cls' if os.name=='nt' else 'clear')
                 for line in transcription:
-                    print(line)
+                    print(line, flush=False)
                 # Flush stdout.
                 print('', end='', flush=True)
 
